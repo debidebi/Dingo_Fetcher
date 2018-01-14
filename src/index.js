f29f415d4f00cae7228af4ca5192b8e44588ce63
Deborah <deborah_huang@hotmail.com>
2018-01-14 15:10:15 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const React = require("react");
+const ReactDom = require("react-dom");
+const axios = require("axios");
+
+const styles = {
+  img: {
+    height: "15em"
+  }
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      imgURL: ""
+    };
+  }
+
+  getRandomImage = () => {
+    axios.get("https://dog.ceo/api/breed/dingo/images/random").then(res => {
+      this.setState({ imgURL: res.data.message });
+    });
+  };
+  componentDidMount() {
+    this.getRandomImage();
+  }
+
+  render() {
+    const { imgURL } = this.state;
+
+    return (
+      <div>
+        <h3> Random Dingo Pictures </h3>
+        <div>
+          <img style={styles.img} alt="" src={imgURL} />
+        </div>
+        <p>
+          <button onClick={this.getRandomImage}>
+            {" "}
+            one more!{" "}
+          </button>
+        </p>
+      </div>
+    );
+  }
+}
+
+ReactDom.render(<App />, document.getElementById("root"));
